bf59ff50284722cb9334a8e3de317c3b2a7fec1c
IsthisAlif <abdulalif2003@gmail.com>
2026-05-21 19:48:34 +0800

    commit 4

---
+++ Tutorial/StringMethods.py
@@ -0,0 +1,16 @@
+# name = input("Enter your full name: ")
+phone_num = input("Enter your phone number: ")
+
+# result = len(name) # Length of the string
+# result = name.find(" ") # Find the index in the string
+# result = name.rfind(" ") # Find the last index in the string
+# name = name.capitalize() # Capitalize the first letter of the string
+# name = name.upper() # Make the string uppercase
+# name = name.lower() # Make the string lowercase
+# result = name.isdigit() # Check if the string is a digit
+# result = name.isalpha() # Check if the string is only letters
+# result = phone_num.count() # Count how many characters are in the string
+# phone_num = phone_num.replace("-", " ") # Replace a character in the string
+
+print(help(str)) # Show all the string methods
+print(phone_num)
