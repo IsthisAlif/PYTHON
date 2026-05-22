fce8a341bdc8119c669cafdc854ee9e0c70802b8
IsthisAlif <abdulalif2003@gmail.com>
2026-05-22 21:24:07 +0800

    commit 6

--- Tutorial/StringIndexing.py
+++ Tutorial/StringIndexing.py
@@ -1,3 +1,14 @@
 # Indexing = accessing elements of a sequence using [] (indexing operator)
 #            [Start : end : step]
 
+credit_number = "1234-5678-9012-3456"
+last_digits = credit_number[15:]
+
+# print(credit_number[0])
+# print(credit_number[0:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-1])
+# print(credit_number[::2])
+
+print(f"XXXX-XXXX-XXXX-{last_digits}")
