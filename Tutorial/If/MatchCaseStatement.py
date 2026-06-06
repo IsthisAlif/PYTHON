40f725c5947b941f7500c3d03d3602d365ced120
IsthisAlif <abdulalif2003@gmail.com>
2026-06-06 23:07:43 +0800

    commit 21

---
+++ Tutorial/If/MatchCaseStatement.py
@@ -0,0 +1,4 @@
+# Match-case statement (switch) = An alternative to using many 'elif' statenents
+#                                 Execute some code if a value matches a 'case'
+#                                 Benefits: cleaner and syntax is more readable
+
