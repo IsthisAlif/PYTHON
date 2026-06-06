2479cd4d4b9d9e8ca3b160f7986a45b824a3dc35
IsthisAlif <abdulalif2003@gmail.com>
2026-06-06 22:45:40 +0800

    commit 19

---
+++ Tutorial/Collection/Iterable.py
@@ -0,0 +1,9 @@
+# Iterable = An object/collection that can return its elements one at a time,
+#            allowing it to be iterated over in a loop 
+
+my_dictionary = {"A" : 1,
+                 "B" : 2,
+                 "C" : 3}
+
+for key, value in my_dictionary.items():
+    print(f"{key} : {value}")
