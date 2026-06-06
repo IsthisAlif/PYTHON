d6e37833d008c57f390b4a7327cf97a1ad012d5d
IsthisAlif <abdulalif2003@gmail.com>
2026-06-06 18:28:50 +0800

    commit 17

---
+++ Tutorial/ArbitraryArgument.py
@@ -0,0 +1,57 @@
+# *args    = allows you to pass multiple non-key arguments
+# **kwargs = allows you to pass multiple keyword-arguments
+#            * unpacking operator
+#            1. positional 2. default 3. keyword 4. ARBITRARY
+
+# 1
+def add(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+
+# 2
+print(add(1, 2, 3, 4))
+
+def display_name(*args):
+    for arg in args:
+        print(arg, end=" ")
+
+display_name("Spongbob", "Harold", "Squarepants")
+print()
+
+# 3
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_address(street="123 Fake St", 
+              apt="100",
+              city="Detroit", 
+              state="Michigan", 
+              zip="54321")
+
+# 4
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+    print()
+    
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get('street')}")
+        print(f"{kwargs.get('pobox')}")
+    else:
+        print(f"{kwargs.get('street')}")
+
+    print(f"{kwargs.get('city')} {kwargs.get('state')}, {kwargs.get('zip')}")
+
+shipping_label("Dr.", "Spongbob", "Squarepant", "3rd",
+               street="123 Fake St",
+               apt="100",
+               pobox="PO box #1001",
+               city="Detroit",
+               state="Michigan",
+               zip="54321")
+
