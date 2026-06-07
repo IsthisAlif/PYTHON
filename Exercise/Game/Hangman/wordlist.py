cc3dadb3589621198ed6f723fdfbe4e084074676
IsthisAlif <abdulalif2003@gmail.com>
2026-06-07 20:05:33 +0800

    commit 28

---
+++ Exercise/Game/Hangman/wordlist.py
@@ -0,0 +1,74 @@
+# Words for hangman game
+
+words = (
+    # Animals
+    "alligator", "antelope", "armadillo", "badger", "bat", "bear", "beaver",
+    "buffalo", "camel", "cat", "cheetah", "chimpanzee", "cobra", "cougar",
+    "coyote", "crocodile", "deer", "dog", "donkey", "dolphin", "eagle",
+    "elephant", "falcon", "ferret", "fox", "frog", "gazelle", "giraffe",
+    "gorilla", "hamster", "hedgehog", "hippopotamus", "horse", "hyena",
+    "jaguar", "kangaroo", "koala", "leopard", "lion", "llama", "lobster",
+    "monkey", "moose", "octopus", "otter", "owl", "panda", "panther",
+    "parrot", "peacock", "penguin", "porcupine", "rabbit", "raccoon",
+    "rhinoceros", "seal", "shark", "sheep", "sloth", "snake", "squirrel",
+    "tiger", "tortoise", "turkey", "walrus", "whale", "wolf", "wombat",
+    "zebra",
+
+    # Fruits & Foods
+    "apple", "apricot", "avocado", "banana", "blackberry", "blueberry",
+    "cabbage", "carrot", "cauliflower", "celery", "cherry", "chocolate",
+    "coconut", "cookie", "cucumber", "donut", "eggplant", "grape",
+    "grapefruit", "hamburger", "kiwi", "lasagna", "lemon", "lettuce",
+    "lime", "mango", "melon", "mushroom", "noodles", "onion", "orange",
+    "papaya", "peach", "peanut", "pear", "pepper", "pineapple", "pizza",
+    "plum", "potato", "pumpkin", "raspberry", "sandwich", "sausage",
+    "spinach", "strawberry", "tomato", "watermelon",
+
+    # Countries
+    "argentina", "australia", "austria", "bangladesh", "belgium",
+    "brazil", "canada", "chile", "china", "colombia", "croatia",
+    "denmark", "egypt", "finland", "france", "germany", "greece",
+    "hungary", "iceland", "india", "indonesia", "ireland", "italy",
+    "japan", "kenya", "malaysia", "mexico", "mongolia", "morocco",
+    "nepal", "netherlands", "newzealand", "nigeria", "norway",
+    "pakistan", "peru", "philippines", "poland", "portugal", "romania",
+    "russia", "singapore", "southafrica", "spain", "sweden",
+    "switzerland", "thailand", "turkey", "ukraine", "vietnam",
+
+    # Technology
+    "algorithm", "android", "application", "artificial", "bandwidth",
+    "browser", "computer", "database", "developer", "encryption",
+    "firewall", "function", "hardware", "internet", "javascript",
+    "keyboard", "laptop", "malware", "memory", "microchip",
+    "monitor", "network", "password", "processor", "programming",
+    "python", "robotics", "scanner", "server", "software",
+    "spreadsheet", "variable", "website",
+
+    # School
+    "biology", "calculator", "chemistry", "classroom", "dictionary",
+    "education", "encyclopedia", "eraser", "geography", "geometry",
+    "grammar", "history", "homework", "language", "library",
+    "literature", "mathematics", "notebook", "pencil", "physics",
+    "principal", "science", "student", "teacher", "university",
+
+    # Sports
+    "archery", "badminton", "baseball", "basketball", "bowling",
+    "boxing", "cricket", "cycling", "football", "golf", "gymnastics",
+    "hockey", "karate", "marathon", "rugby", "skateboarding",
+    "snowboarding", "soccer", "swimming", "tennis", "volleyball",
+    "wrestling",
+
+    # Nature
+    "avalanche", "canyon", "desert", "earthquake", "forest",
+    "glacier", "hurricane", "island", "jungle", "lagoon",
+    "lightning", "mountain", "oasis", "ocean", "rainbow",
+    "river", "thunder", "tornado", "valley", "volcano",
+    "waterfall", "wildfire",
+
+    # Hard Words
+    "awkward", "camouflage", "cryptography", "difficult",
+    "electricity", "extraordinary", "headquarters", "jigsaw",
+    "knowledge", "labyrinth", "magnificent", "mnemonic",
+    "nightmare", "pneumonia", "quicksand", "rhythm",
+    "submarine", "xylophone", "yesterday", "zookeeper"
+)
