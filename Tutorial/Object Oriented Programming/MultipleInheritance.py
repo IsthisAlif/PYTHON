2eb6ebbab6e74b6224220c9663058549b74e335a
IsthisAlif <abdulalif2003@gmail.com>
2026-06-10 22:50:47 +0800

    commit 37

--- Tutorial/Object Oriented Programming/MultipleInheritance.py
+++ Tutorial/Object Oriented Programming/MultipleInheritance.py
@@ -3,3 +3,39 @@
 
 # multilevel inheritance = inherit from a parent which inherits from another parent
 #                          C(B) <- B(A) <- A
+class Animals:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleeping(self):
+        print(f"{self.name} is sleeping")
+class Prey(Animals):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(Animals):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Bugs")
+hawk = Hawk("Tony")
+fish = Fish("Nemo")
+
+fish.flee()
+fish.hunt()
+
+rabbit.eat()
+
+hawk.sleep()
